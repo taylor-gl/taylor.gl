5f3677863d5b11d9c4e6965062a2dfab3ac6e4c0
Taylor G. Lunt <taylor@taylor.gl>
2024-02-05 22:01:08 -0500

    Include Writings in RSS feed

--- lib/blog_new/blog/rss.ex
+++ lib/blog_new/blog/rss.ex
@@ -3,6 +3,7 @@ defmodule BlogNew.Blog.RSS do
   alias BlogNew.Repo
   alias BlogNew.Blog.Post
   alias BlogNew.Blog.ElixirRSS
+  alias BlogNew.Writing
 
   @rss_item_desc_length 240
 
@@ -24,20 +25,22 @@ defmodule BlogNew.Blog.RSS do
         "en-us"
       )
 
-    # build RSS items from database (do not include draft posts)
-    query =
+    # build RSS items from database (do not include draft posts or draft writings)
+    posts_query =
       from p in Post,
-        where: p.draft == false
+      where: p.draft == false
 
-    items =
-      Repo.all(query)
-      |> Enum.sort(&Post.sort_posts/2)
-      |> Enum.map(&post_to_rss_item(&1))
+    writings_query = from w in Writing, where: w.draft == false
 
-    ElixirRSS.feed(channel, items)
+    posts = Repo.all(posts_query)
+    writings = Repo.all(writings_query)
+
+    sorted_items = (posts ++ writings) |> sort_items() |> Enum.map(&to_rss_item/1)
+
+    ElixirRSS.feed(channel, sorted_items)
   end
 
-  defp post_to_rss_item(%Post{
+  defp to_rss_item(%Post{
          plain_content: plain_content,
          title: title,
          publish_date: publish_date,
@@ -49,9 +52,34 @@ defmodule BlogNew.Blog.RSS do
     publish_datetime = DateTime.new!(publish_date, ~T[00:00:00], "GMT")
     publish_date_RFC1123 = Timex.format!(publish_datetime, "{RFC1123}")
 
-    link = "https://taylor.gl/post/#{Post.post_id(markdown_filename)}"
+    link = "https://taylor.gl/blog/#{Post.post_id(markdown_filename)}"
 
     # using link as the guid
     ElixirRSS.item(title, description, publish_date_RFC1123, link, link)
+  end
+
+  defp to_rss_item(%Writing{
+        plain_content: plain_content,
+        title: title,
+        publish_date: publish_date,
+        markdown_filename: markdown_filename
+                   }) do
+    # to form an RSS description, remove HTML tags from content, and then truncate to appropriate length
+    # using regex to remove HTML tags from RSS is imperfect, but I think it will be fine in this case
+    description = String.slice(plain_content, 0, @rss_item_desc_length - 3) <> "..."
+    publish_datetime = DateTime.new!(publish_date, ~T[00:00:00], "GMT")
+    publish_date_RFC1123 = Timex.format!(publish_datetime, "{RFC1123}")
+
+    link = "https://taylor.gl/creative-writing/#{Writing.slug(markdown_filename)}"
+
+    # using link as the guid
+    ElixirRSS.item(title, description, publish_date_RFC1123, link, link)
+  end
+
+  defp sort_items(items) do
+    items
+    |> Enum.sort(fn a, b ->
+      Date.compare(a.publish_date, b.publish_date) == :gt
+    end)
   end
 end
